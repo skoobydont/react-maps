165640846f4250c7b938a41ff59a2614e67d007a
skoobydont <31373232+skoobydont@users.noreply.github.com>
2020-08-23 17:15:25 -0400

    map init at norris dam

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,29 @@
 import React from 'react';
-import Map from './Map';
-import logo from './logo.svg';
 import './App.css';
+import { withGoogleMap, GoogleMap, withScriptjs } from 'react-google-maps';
+
+const Map = () => {
+    return(
+        <GoogleMap
+            defaultZoom={15}
+            defaultCenter={{ lat: 36.2254975, lng: -84.0972329 }}
+        />
+    )
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 const App = () => {
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-      <div>
-        Map Section
-        <Map />
-      </div>
+    <div className="App" style={{ width: '100vw', height: '100vw'}}>
+      Map Section
+      <WrappedMap 
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_API_KEY}`}
+        loadingElement={<div style={{ height: "100%"}}/>}
+        containerElement={<div style={{ height: "100%"}}/>}
+        mapElement={<div style={{ height: "100%"}}/>}        
+      />
     </div>
   );
 }
